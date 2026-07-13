c235250e20cb668cb5a6a3548201a8f1e42bb2df
Nnaemeka Obioha <nnaemeka.obioha@gmail.com>
2026-07-12 23:03:17 -0400

    base commit

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { createApp } from '../src/app';
-import { initDb } from '../src/db';
-
-const app = createApp();
-
-initDb().catch(err => console.error('[api] initDb failed:', err));
-
-export default app;
